e33293cfde2ed86977cf0dc15e890fe5c125bdd7
Mike Menne <mike@humanize.org>
2018-08-24 16:01:21 -0500

    Update testing.rb

--- lib/migration_data/testing.rb
+++ lib/migration_data/testing.rb
@@ -2,7 +2,7 @@ require 'rails'
 
 def require_migration(migration_name)
   path = MigrationData::ActiveRecord::Migration.migration_dir
-  all_migrations = ActiveRecord::Migrator.migrations(path)
+  all_migrations = ActiveRecord::MigrationContext.new(path).migrations
 
   migration_name += '.rb' unless migration_name.end_with?('.rb')
   file = all_migrations.detect do |m|
